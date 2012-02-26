71ae240807f6789b33689e1a197fabd3a41df910
spencer <sepmein@gmail.com>
2012-02-26 22:38:39 +0800

    A new webChat using websocket!!

--- server.js
+++ server.js
@@ -1,39 +1,90 @@
-//read file from disk
-//static servers
-//now just response the index.js file to the request
-
-
-var http = require('http');
-var fs = require('fs');
+var STATICPATH = __dirname + '/public';
+var mime = require('mime');
 var url = require('url');
+var fs = require('fs');
+var querystring = require('querystring');
+
+var app = require('http').createServer(handler)
+    , io = require('socket.io').listen(app);
+
+io.configure('production',function(){
+  io.enable('browser client minification');  // send minified client
+  io.enable('browser client etag');          // apply etag caching logic based on version number
+  io.enable('browser client gzip');          // gzip the file
+  io.set('log level', 1);                    // reduce logging
+  io.set('transports', [                     // enable all transports (optional if you want flashsocket)
+      'websocket'
+    , 'flashsocket'
+    , 'htmlfile'
+    , 'xhr-polling'
+    , 'jsonp-polling'
+  ]);
+});
 
 
-//	console.log(__dirname);
-//var requestUrl = url.parse(request.url).pathname
-//var filepath = __dirname + requestUrl;
 
-//	console.log(filepath);
+var messages = [];
+var MAX = 20;
 
-fs.stat(__dirname + '/server.js',function(err, stat){
-	if (err) {
-		throw err;
-	}
+app.listen(4000);
 
-	http.createServer(function(request , response){
+function handler(req,res){
 
-		response.writeHead(200,{
-			'content-type' : 'text/plain' , 
-			'content-length' : stat.size
-		});
+  //simple router
 
-		console.log(stat.size);
+    var pathname = STATICPATH + url.parse(req.url).pathname;
 
-		var data = fs.readFile(__dirname + '/server.js',function(err, data){
+    console.log("request pathname = " + pathname);
 
-			response.end(data);
+    if (pathname === STATICPATH + '/') {
 
-		});
+      pathname = STATICPATH + '/index.html';
 
-	}).listen(80);
+    }
 
+    var getMime = mime.lookup(pathname);
+
+
+  //response section
+
+  //get content-length by fs.stat
+    fs.stat(pathname,function(err,stat){
+      if(err){
+        console.log(err);
+        res.writeHead(404);
+        res.end('No Such File error');
+        return;
+      }
+
+      res.writeHead(200,{
+        "content-type" : getMime,
+        "content-length" : stat.size
+      });
+
+      fs.readFile(pathname,function(err,data){
+        if(err){
+          throw err;
+        }
+        res.end(data);
+
+      });
+
+    });
+
+}
+
+io.sockets.on('connection', function (socket) {
+
+  socket.emit('newComer', messages );
+
+  socket.on('say', function (data) {
+    console.log(data);
+    if (messages.length === MAX) {
+      messages.shift();    
+    }
+    messages.push(data);
+
+    socket.emit('newMessage',messages[messages.length-1]);
+    socket.broadcast.emit('newMessage',messages[messages.length-1]);
+  });
 });
