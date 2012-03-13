a14f3df287ed14823b38d2bc7906b5c700ddaa86
spencer <sepmein@gmail.com>
2012-03-13 18:49:40 +0800

    ready for kokiya.no.de

--- public/js/script.js
+++ public/js/script.js
@@ -12,7 +12,7 @@ var Message = function(content, timeStamp) {
 	};
 var user = '';
 
-var socket = io.connect('http://192.168.9.155:8080');
+var socket = io.connect('http://kokiya.no.de');
 
 //将返回数据解析成html，这个部分应该就是传说中的view了，应该想办法把它抽象化。
 function getView(object){
