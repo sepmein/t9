63444a423616598e87070975670a87163be410be
sepmein <sepmein@gmail.com>
2012-05-17 10:06:20 +0800

    Building coupon sys, finished requireCoupon part

--- handlers/coupon.js
+++ handlers/coupon.js
@@ -6,8 +6,15 @@ var coupon = coupon || {};
 coupon.requireCoupon = function(email) {
 	if(!validate(email)) {
 		//email 不符合要求
+
 	} else {
-		db.coupons.add(email, )
+		db.coupons.add(email, function(status, err){
+			if (status.ok) {
+				//success
+			} else {
+				console.log("ERR requiring coupon :"+err);
+			}
+		});
 	}
 }
 
