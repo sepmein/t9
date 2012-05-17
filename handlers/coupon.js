542d4af3303f02e46d7c93aa945dc9dbdfca2977
sepmein <sepmein@gmail.com>
2012-05-17 14:15:14 +0800

    Seperated util module, Updated jQuery

--- handlers/coupon.js
+++ handlers/coupon.js
@@ -1,18 +1,22 @@
 var db = db || {};
 db.coupons = require('.././data/coupon');
 
+var validateEmail = require('.././util').validateEmail;
+
 var coupon = coupon || {};
 
-coupon.requireCoupon = function(email) {
-	if(!validate(email)) {
+coupon.requireCoupon = function(req, res, next) {
+	var email = req.body.email;
+	if (!validateEmail(email)) {
 		//email 不符合要求
-
+		console.log('ERR requiring coupon : email validation err');
 	} else {
-		db.coupons.add(email, function(status, err){
+		db.coupons.add(email, function(status, err) {
 			if (status.ok) {
 				//success
+				console.log('Email: ' + email + ' has been added to the que');
 			} else {
-				console.log("ERR requiring coupon :"+err);
+				console.log("ERR requiring coupon :" + err);
 			}
 		});
 	}
