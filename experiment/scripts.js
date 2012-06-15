f835cb6abf44a40f5e2ea6a198fa584d67958208
sepmein <sepmein@gmail.com>
2012-06-15 16:49:34 +0800

    Improved star magnitude

--- experiment/scripts.js
+++ experiment/scripts.js
@@ -1,17 +1,32 @@
 (function() {
 	var holder = document.getElementById('holder');
-	var paper = Raphael(holder, 1000, 1000);
-	/*	var circle = paper.circle(0, 0, 50);
-	var path = paper.path('m50,50c0,40,50,40,50,0');
-	var path1 = paper.path('m50,100s25,70,50,0');
-	var path2 = paper.path('m50,150q25,70,50,0');
-	var path3 = paper.path('m50,200t50,0t0,50z');
-	var e = paper.path('m50,300a50,50,0,0,1,100,0');*/
+	var paper = Raphael(holder, 2000, 2000);
+
 	var preference = {
 		P: [400, 400],
 		ALPHA: 30,
 		MAXRANGE: 900,
-		NUMOFTRACE: 700
+		NUMOFTRACE: 1000,
+		MAGNITUDE: {
+			ONE: {
+				num: 10
+			},
+			TWO: {
+				num: 23
+			},
+			THREE: {
+				num: 67
+			},
+			FOUR: {
+				num: 229
+			},
+			FIVE: {
+				num: 738
+			},
+			SIX: {
+				num: 2420
+			}
+		}
 	};
 
 	function starTrace(P) {
@@ -23,27 +38,45 @@
 			return coords;
 		}
 
+		function magnitude(){
+			var m = {},
+			grade = Math.random();
+			m.opacity = grade;
+			m.glowWidth = grade*5;
+			return m;
+		}
+
 		function getRandomSVGString() {
 			var origin = P.P,
 				degree = Math.random() * 360,
 				distance = Math.random() * P.MAXRANGE;
 			var coords = [getCoords(origin, degree, distance), getCoords(origin, degree + P.ALPHA, distance)];
-			console.log(coords);
+			//console.log(coords);
 			var svg = 'm' + coords[0][0] + ',' + coords[0][1] + 'a' + distance + ',' + distance + ',0,0,1,' + (coords[1][0] - coords[0][0]) + ',' + (coords[1][1] - coords[0][1]);
-			console.log(svg);
+			//console.log(svg);
 			return svg;
 		}
-		//testing
-		/*var point = [getCoords([300, 300], -30, 200), getCoords([300, 300], 0, 200)];
-	console.log(point);
-	paper.path('m' + P.P[0] + ',' + P.P[0] + 'l' + (point[0][0] - 300) + ',' + (point[0][1] - 300) + 'a' + '300,300,0,0,1,' + (point[1][0] - point[0][0]) + ',' + (point[1][1] - point[0][1]));*/
 
 		for (var i = P.NUMOFTRACE; i--; i > 0) {
 			paper.path(getRandomSVGString());
 		}
+
+		paper.forEach(function(el) {
+			var m = magnitude();
+			el.attr({
+				stroke: "white"
+			});
+			el.glow({
+				width: m.glowWidth,
+				color: 'grey',
+				opacity: m.opacity
+			});
+			el.attr({
+				opacity: m.opacity
+			});
+		});
 	}
 
 	starTrace(preference);
-
 
 })();
