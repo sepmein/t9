6b54f0221383e52a2216b649f95cd89fb3e2e3f4
Spencer <sepmein@gmail.com>
2012-06-18 22:39:09 +0800

    WOWWOWOWOW! BIG BANG!

--- experiment/scripts.js
+++ experiment/scripts.js
@@ -1,12 +1,12 @@
 (function(){
 	var holder = document.getElementById('holder');
-	var paper = Raphael(holder, 1280, 600);
+	var paper = Raphael(holder, 1280, 800);
 
 	var preference = {
-		P: [400, 250],
+		P: [640, 400],
 		ALPHA: 30,
 		MAXRANGE: 1000,
-		NUMOFTRACE: 3000,
+		NUMOFTRACE: 700,
 		MAGNITUDE: {
 			NUM: [10,23,67,229,738,2420],
 			LIGHT: [1,1/1.4,1/1.4/1.4,1/1.4/1.4/1.4,1/1.4/1.4/1.4/1.4,1/1.4/1.4/1.4/1.4/1.4,1/1.4/1.4/1.4/1.4/1.4/1.4]
@@ -15,11 +15,34 @@
 
 	function starTrace(P) {
 
-		function getCoords(origin, degree, distance) {
-			var coords = [];
-			var radians = (degree / 180) * Math.PI;
-			coords[0] = origin[0] + Math.cos(radians) * distance;
-			coords[1] = origin[1] + Math.sin(radians) * distance;
+		paper.customAttributes.arc = function(origin, degree, distance, alpha) {
+
+			var coords = getCoords(origin, degree, distance, alpha);
+
+			var path = [
+					['m',coords[0][0],coords[0][1]],
+					['a',distance,distance,0,0,1,(coords[1][0] - coords[0][0]),(coords[1][1] - coords[0][1])]
+				];
+
+			var m = magnitude();
+
+			return {
+				path:path,
+				stroke: "white",
+				"stroke-width": m.strokeWidth,
+				opacity: m.opacity
+			};
+		};
+
+		function getCoords(o, d, dis, al) {
+			var coords = [[],[]];
+			var radians = (d / 180) * Math.PI;
+			var radiansPlus = ( (d + al) / 180) * Math.PI;
+			coords[0][0] = o[0] + Math.cos(radians) * dis;
+			coords[0][1] = o[1] + Math.sin(radians) * dis;
+			coords[1][0] = o[0] + Math.cos(radiansPlus) * dis;
+			coords[1][1] = o[1] + Math.sin(radiansPlus) * dis;
+			//console.log(coords);
 			return coords;
 		}
 
@@ -53,7 +76,7 @@
 			return m;
 		}
 
-		function getRandomSVGString() {
+/*		function getRandomSVGString() {
 			var origin = P.P,
 				degree = Math.random() * 360,
 				distance = Math.random() * P.MAXRANGE;
@@ -63,24 +86,38 @@
 			//console.log(svg);
 			return svg;
 		}
-
+*/
 		for (var i = P.NUMOFTRACE; i--; i > 0) {
-			paper.path(getRandomSVGString());
+			var distance = Math.random() * P.MAXRANGE,
+				degree = Math.random() * 360,
+				al = P.ALPHA;
+
+			paper.path().attr({arc:[P.P, degree, distance, al]});
 		}
 
-		paper.forEach(function(el) {
+		/*paper.forEach(function(el) {
 			var m = magnitude();
 			el.attr({
 				stroke: "white",
 				"stroke-width": m.strokeWidth,
 				opacity: m.opacity
-			});
+			});*/
 			/*el.glow({
 				width: m.glowWidth,
 				color: 'grey',
 				opacity: m.opacity
 			});*/
-		});
+		//});
+		setInterval( function(){
+			paper.forEach(function(el) {
+				//console.log(el);
+				var d = el.attrs.arc[1],
+					dis = el.attrs.arc[2],
+					al = el.attrs.arc[3] + 10;
+				//console.log(dis);
+				el.animate({arc:[P.P, d, dis, al]},1000);
+			})
+		},1000);
 	}
 
 	starTrace(preference);
