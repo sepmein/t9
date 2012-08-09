c3aaabf9bd6e5d55c24b6607976941915d9e8b1c
Spencer <sepmein@gmail.com>
2012-08-10 00:56:19 +0800

    Changed vec/index styles

--- public/js/vec/scripts.js
+++ public/js/vec/scripts.js
@@ -1,20 +1,20 @@
 (function() {
 	var holder = document.getElementById('holder');
-	var paper = Raphael(holder, 300, 200);
+	var paper = Raphael(holder, 300, 150);
 	paper.customAttributes.along = function(a) {
 		return {
 			path: logo.getSubpath(0, a)
 		};
 	};
 	var set = paper.set();
-	var logo = paper.path('M10 10L45 100L80 10M140 12A44 44 0 1 0 140 98M87 55H250M210 12A44 44 0 1 0 210 98').attr({
+	var logo = paper.path('M40 10L75 100L120 10M170 12A44 44 0 1 0 170 98M117 55H280M240 12A44 44 0 1 0 240 98').attr({
 		opacity: 0,
 		stroke: 'rgb(100,100,100)'
 	});
 	var c = paper.path().attr({
 		along: 0,
-		'stroke-width': '2',
-		stroke: 'rgb(100,100,100)'
+		'stroke-width': '3',
+		stroke: 'rgb(225,225,225)'
 	});
 	paper.rect(0, 0, 600, 120).attr({
 		fill: '#000',
