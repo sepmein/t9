89d802617e990e0b72331b0c3521b1fd493ea4e0
sepmein <sepmein@gmail.com>
2012-08-17 16:43:28 +0800

    Designing & ing output.jade

--- public/js/vec/start.js
+++ public/js/vec/start.js
@@ -15,28 +15,21 @@ if ($('form').length && $('form')[0].checkValidity) {
 //fancy logo!!
 (function() {
 	var holder = document.getElementById('holder');
-	var paper = Raphael(holder, 600, 120);
+	var paper = Raphael(holder, 300, 150);
 	paper.customAttributes.along = function(a) {
 		return {
 			path: logo.getSubpath(0, a)
 		};
 	};
 	var set = paper.set();
-	var logo = paper.path('M10 10L45 100L80 10M140 12A44 44 0 1 0 140 98M87 55H250M210 12A44 44 0 1 0 210 98').attr({
+	var logo = paper.path('M40 10L75 100L120 10M170 12A44 44 0 1 0 170 98M117 55H280M240 12A44 44 0 1 0 240 98').attr({
 		opacity: 0,
 		stroke: 'rgb(100,100,100)'
 	});
-	var subtitle = paper.text(375, 76, 'hands-free').attr({
-		'font-size': '60px',
-		'font-family': 'Helvetica Neue',
-		'font-weight': '100',
-		'fill': 'rgb(125,125,125)',
-		opacity: 0
-	});
 	var c = paper.path().attr({
 		along: 0,
-		'stroke-width': '2',
-		stroke: 'rgb(100,100,100)'
+		'stroke-width': '5',
+		stroke: '#268ABB'
 	});
 	paper.rect(0, 0, 600, 120).attr({
 		fill: '#000',
@@ -51,10 +44,8 @@ if ($('form').length && $('form')[0].checkValidity) {
 	setTimeout(function() {
 		c.animate({
 			along: logo.getTotalLength()
-		}, 5000, function() {
-			subtitle.animate({
-				opacity: 1
-			}, 300);
+		}, 5000,function(){
+			$('p.speech').show('fast');
 		});
 	}, 500);
 })();
