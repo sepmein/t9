00b09d2bb0d43bdbdc1476b0f86347a2cf1ab1a3
Spencer <sepmein@gmail.com>
2012-08-20 23:53:45 +0800

    Protected myself

--- public/js/vec/start.js
+++ public/js/vec/start.js
@@ -1,13 +1,27 @@
-if ($('form').length && $('form')[0].checkValidity) {
-	console.log('support checkValidity');
-	$('#sub').addClass('disabled');
-	$('input').keyup(function() {
-		if ($('form')[0].checkValidity()) {
-			$('#sub').removeClass('disabled');
-		} else {
-			$('#sub').addClass('disabled');
+(function() {
+
+	if ($('form').length && $('form')[0].checkValidity) {
+		console.log('support checkValidity');
+		$('#sub').addClass('disabled');
+		$('input').keyup(function() {
+			if ($('form')[0].checkValidity()) {
+				$('#sub').removeClass('disabled');
+			} else {
+				$('#sub').addClass('disabled');
+			}
+		});
+	} else {
+		console.log('unsupport');
+	}
+
+	$('#sub').click(function() {
+		$('form')[0].checkValidity();
+		if (!$('#sub').hasClass('disabled')) {
+			$('#alert').modal({
+				'backdrop': 1,
+				'keyboard': 1
+			});
 		}
 	});
-} else {
-	console.log('unsupport');
-}
+
+})();
