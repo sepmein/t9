2eba9452815c33b731a1ace2e6ff3085015eb7ab
Spencer <sepmein@gmail.com>
2012-08-22 00:31:25 +0800

    added some docs

--- util/index.js
+++ util/index.js
@@ -1,5 +1,15 @@
+/**
+ * kokiya 功能包，为其他模块提供快捷程序
+ * @module util
+ */
 var util = util || {};
 
+/**
+ * 验证邮件地址，使用正则表达式
+ * @method validateEmail
+ * @param {String} email Email
+ * @return {Boolean} 成功返回1，否则返回0
+ */
 util.validateEmail = function(email) {
 	var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 	return re.test(email);
